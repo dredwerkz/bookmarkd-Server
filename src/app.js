7fb2ac771dd433b466a367a3ecc0af4a2a7a30dd
Ameenah Jalil <ameenah_234@icloud.com>
2023-12-15 09:57:29 +0000

    Installed dotenv and Open Ai

--- src/app.js
+++ src/app.js
@@ -2,8 +2,14 @@ import express from "express";
 import morgan from "morgan";
 import { bookmarkdRouter } from "./data/router.js";
 import cors from "cors";
+import OpenAI from "openai";
+import * as dotenv from "dotenv";
 
 const app = express();
+dotenv.config()
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+})
 
 app.use(cors({
         origin: '*' // Actually secure this at some point
@@ -12,7 +18,8 @@ app.use(cors({
 app.use(morgan("dev"));
 app.use(express.json());
 app.use("/api/", bookmarkdRouter);
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use("/aiapi",bookmarkdRouter );
 
 export default app;
 
