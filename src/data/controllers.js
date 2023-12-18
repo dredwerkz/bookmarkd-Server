066bbe19113a22db41e22df9319f8c0b2bafa862
dredwerkz <60428436+dredwerkz@users.noreply.github.com>
2023-12-18 15:03:57 +0000

    Added search func

--- src/data/controllers.js
+++ src/data/controllers.js
@@ -4,7 +4,7 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+    apiKey: process.env.OPENAI_API_KEY,
 });
 
 // This file controls the api request, then repackages it for the router to send back
@@ -14,14 +14,14 @@ const openai = new OpenAI({
 /** ////////////////////////////////////////////////////////////////////////////////////////////// */
 
 export async function getAllUsers(req, res) {
-  console.log(`getAllUsers() in controllers.js has been called`);
-  const users = await apiRequests.getAllUsers();
+    console.log(`getAllUsers() in controllers.js has been called`);
+    const users = await apiRequests.getAllUsers();
 
-  res.status(200).json({
-    success: true,
-    payload: users,
-    // check this later
-  });
+    res.status(200).json({
+        success: true,
+        payload: users,
+        // check this later
+    });
 }
 
 /** //////////////////////////////////////////////////////////////////////////////////////////////
@@ -29,16 +29,16 @@ export async function getAllUsers(req, res) {
 /** ////////////////////////////////////////////////////////////////////////////////////////////// */
 
 export async function getSpecificUser(req, res) {
-  console.log(
-    `getSpecificUser was called looking for user with id ${req.query["user_id"]}`
-  );
-  const desiredUserId = req.query["user_id"];
-  const user = await apiRequests.getSpecificUser(desiredUserId);
+    console.log(
+        `getSpecificUser was called looking for user with id ${req.query["user_id"]}`
+    );
+    const desiredUserId = req.query["user_id"];
+    const user = await apiRequests.getSpecificUser(desiredUserId);
 
-  res.status(200).json({
-    success: true,
-    payload: user,
-  });
+    res.status(200).json({
+        success: true,
+        payload: user,
+    });
 }
 
 /** //////////////////////////////////////////////////////////////////////////////////////////////
@@ -46,32 +46,32 @@ export async function getSpecificUser(req, res) {
 /** ////////////////////////////////////////////////////////////////////////////////////////////// */
 
 export async function getBookshelves(req, res) {
-  /* 
+    /* 
     I want to take in an array of bookshelf IDs as part of the req.query, 
     convert them to a usable format for our router, 
     which will fetch the relevant bookshelves as an array of objects?
     */
-  if (req.query["user_id"]) {
-    console.log(`Get Bookshelves API route called with a user_id query`);
+    if (req.query["user_id"]) {
+        console.log(`Get Bookshelves API route called with a user_id query`);
 
-    const fetchedBookshelves = await apiRequests.getBookshelves(
-      req.query["user_id"]
-    );
+        const fetchedBookshelves = await apiRequests.getBookshelves(
+            req.query["user_id"]
+        );
 
-    res.status(200).json({
-      success: true,
-      payload: fetchedBookshelves,
-    });
-  } else {
-    console.log(
-      `Get Bookshelves API route called with an invalid or missing user_id query`
-    );
-    return res.status(400).json({
-      success: false,
-      payload: null,
-      message: "You must include a search query for this function",
-    });
-  }
+        res.status(200).json({
+            success: true,
+            payload: fetchedBookshelves,
+        });
+    } else {
+        console.log(
+            `Get Bookshelves API route called with an invalid or missing user_id query`
+        );
+        return res.status(400).json({
+            success: false,
+            payload: null,
+            message: "You must include a search query for this function",
+        });
+    }
 }
 
 /** //////////////////////////////////////////////////////////////////////////////////////////////
@@ -79,14 +79,14 @@ export async function getBookshelves(req, res) {
 /** ////////////////////////////////////////////////////////////////////////////////////////////// */
 
 export async function getBook(req, res) {
-  console.log(`getBook() in controllers.js has been called`);
-  const book = await apiRequests.getBook(req.query["book_id"]);
+    console.log(`getBook() in controllers.js has been called`);
+    const book = await apiRequests.getBook(req.query["book_id"]);
 
-  res.status(200).json({
-    success: true,
-    payload: book,
-    // check this later
-  });
+    res.status(200).json({
+        success: true,
+        payload: book,
+        // check this later
+    });
 }
 
 /** //////////////////////////////////////////////////////////////////////////////////////////////
@@ -94,44 +94,62 @@ export async function getBook(req, res) {
 /** ////////////////////////////////////////////////////////////////////////////////////////////// */
 
 export async function getBookData(req, res) {
-  console.log(
-    `getBookData() has been called with user_id ${req.query["user_id"]} and book_id ${req.query["book_id"]} `
-  );
-  const bookData = await apiRequests.getBookData(
-    req.query["user_id"],
-    req.query["book_id"]
-  );
+    console.log(
+        `getBookData() has been called with user_id ${req.query["user_id"]} and book_id ${req.query["book_id"]} `
+    );
+    const bookData = await apiRequests.getBookData(
+        req.query["user_id"],
+        req.query["book_id"]
+    );
 
-  res.status(200).json({
-    success: true,
-    payload: bookData,
-  });
+    res.status(200).json({
+        success: true,
+        payload: bookData,
+    });
 }
 /** //////////////////////////////////////////////////////////////////////////////////////////////
 /** Get AI data 
 /** ////////////////////////////////////////////////////////////////////////////////////////////// */
 
 export async function getAiRec(req, res) {
-  // console.log(`getBookData() has been called with user_id ${req.query["user_id"]} and book_id ${req.query["book_id"]} `);
-  const typeOfResponse =
-    " In a list of 3 book recommandations, give me the author:";
-  const prompt = req.body.prompt;
-  // const prompt = "JK Rowling"
-  const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "user",
-        // Example: `${"In a list of 3 book recommandations, give me the author: "}, ${" Jk Rowling "}
-        content: `${typeOfResponse}, ${prompt}`,
-      },
-    ],
-    // How long the response will be: 150, 200, 250, 300, 350, 400, 450, 500, 550, 600
-    max_tokens: 450,
-  });
+    // console.log(`getBookData() has been called with user_id ${req.query["user_id"]} and book_id ${req.query["book_id"]} `);
+    const typeOfResponse =
+        " In a list of 3 book recommandations, give me the author:";
+    const prompt = req.body.prompt;
+    // const prompt = "JK Rowling"
+    const response = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [
+            {
+                role: "user",
+                // Example: `${"In a list of 3 book recommandations, give me the author: "}, ${" Jk Rowling "}
+                content: `${typeOfResponse}, ${prompt}`,
+            },
+        ],
+        // How long the response will be: 150, 200, 250, 300, 350, 400, 450, 500, 550, 600
+        max_tokens: 450,
+    });
 
-  res.status(200).json({
-    success: true,
-    payload: response.choices[0].message,
-  });
+    res.status(200).json({
+        success: true,
+        payload: response.choices[0].message,
+    });
+}
+
+/** //////////////////////////////////////////////////////////////////////////////////////////////
+/** Get search results
+/** ////////////////////////////////////////////////////////////////////////////////////////////// */
+
+export async function getSearchResults(req, res) {
+    console.log(
+        `getSearchResults() has been called with search query ${req.query["query"]}`
+    );
+    const searchResults = await apiRequests.getSearchResults(
+        req.query["search"]
+    );
+
+    res.status(200).json({
+        success: true,
+        payload: searchResults,
+    });
 }
